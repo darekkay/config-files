e5d9578741e1921c961dbe97d4c8c64d90f1ecfe
Darek Kay <darekkay@gmail.com>
2017-08-01 22:38:42 +0200

    Add Chrome features and snippets

---
+++ chrome/README.md
@@ -0,0 +1,11 @@
+# Chrome
+
+ - Open Command Menu: `Ctrl + Shift + P`
+ - [Tabluar object view](https://umaar.com/dev-tips/82-console-table/) with `console.table(object)`
+ - [Toggle element classes](https://umaar.com/dev-tips/87-toggle-element-classes/)
+ - [Quick change CSS values](https://umaar.com/dev-tips/37-quick-change-css-values/):
+   - Up / Down: increment / decrement by 1
+   - Shift + Up: increment by 10
+   - Alt + Up: increment by 0.1
+ - [DevTools snippets](https://umaar.com/dev-tips/141-quick-open-menu-snippets/)
+ - [Blackbox scripts](https://umaar.com/dev-tips/128-blackboxing/)
---
+++ chrome/snippets/jquerify.js
@@ -0,0 +1,23 @@
+// jquerify.js
+// https://github.com/bgrins/devtools-snippets
+// Add jQuery to any page that does not have it already.
+
+(function () {
+
+  if ( !window.jQuery ) {
+    var dollarInUse = !!window.$;
+    var s = document.createElement('script');
+    s.setAttribute('src', 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js');
+    s.addEventListener('load', function(){
+      console.log('jQuery loaded!');
+
+      if(dollarInUse) {
+        jQuery.noConflict();
+        console.log('`$` already in use; use `jQuery`');
+      }
+    });
+
+    document.body.appendChild(s);
+  }
+
+})();
