0a0192140875e1ddc21b63b481d40e49e9b0e732
Darek Kay <darekkay@gmail.com>
2017-12-16 17:01:38 +0100

    Add prettier settings

---
+++ prettier/README.md
@@ -0,0 +1,9 @@
+# Prettier
+
+- To use *global settings*, put `.prettierrc` in a folder above the projects (not the home directory, except if the projects are located inside it).
+- The [VSCode plugin](https://marketplace.visualstudio.com/items?itemName=esbenp.prettier-vscode) caches prettier settings, so VSCode has to be restarted after any changes.
+- Npm script:
+
+```
+  "format": "prettier --write -- \"{src, public}/**/*.{js,jsx,json,css}\""
+```
---
+++ prettier/example.js
@@ -0,0 +1,12 @@
+/**
+ * Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vestibulum ex eget quam varius, vel tincidunt ligula placerat.
+ */
+
+const func = moo => {
+  console.log(`Cow says: ${moo}`);
+};
+
+const loremIpsumVariable080 = "[ 80] Lorem ipsum dolor sit amet, consectetur.";
+const loremIpsumVariable100 = "[100] Lorem ipsum dolor sit amet, consectetur adipiscing elit cra.";
+const loremIpsumVariable120 =
+  "[120] Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vestibulum ex eget";
