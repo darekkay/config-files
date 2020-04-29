5af0ee359b38972bea81fce43d141a0b56ed8ea6
Darek Kay <darekkay@gmail.com>
2020-04-29 18:31:52 +0200

    Re-enable the date parser because of tag issues

--- userscripts/todoist-enhancements.user.js
+++ userscripts/todoist-enhancements.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            Todoist Enhancements
-// @version         1.4.3
-// @description     Brings back square checkboxes, former priority colors and turns off the date parser
+// @version         1.4.4
+// @description     Brings back square checkboxes and former priority colors
 // @author          Darek Kay <hello@darekkay.com>
 // @namespace       https://darekkay.com
 // @downloadURL     https://github.com/darekkay/config-files/raw/master/userscripts/todoist-enhancements.user.js
@@ -18,9 +18,3 @@ GM_addStyle(`
   .priority_3 .ist_checkbox { background-color: #0063a6 !important; border-color: #00548c !important; }
   .priority_4 .ist_checkbox { background-color: #d24726 !important; border-color: #b33d20 !important; }
 `);
-
-waitForClass("richtextinput_match", removeDate);
-
-function removeDate(node) {
-  $(node).click();
-}
