0036c5da78598ec179cd805be4785139f09f2876
Darek Kay <hello@darekkay.com>
2021-01-03 19:52:45 +0100

    Revert IMDB copy script back to markdown

--- userscripts/imdb-copy.user.js
+++ userscripts/imdb-copy.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name            IMDB Copy
-// @version         1.0.2
+// @version         1.0.3
 // @description     Create a TODO out of the current IMDB site
 // @author          Darek Kay <hello@darekkay.com>
 // @namespace       https://darekkay.com
-// @downloadURL     https://github.com/darekkay/config-files/raw/master/userscripts/imdb-to-link.user.js
+// @downloadURL     https://github.com/darekkay/config-files/raw/master/userscripts/imdb-copy.user.js
 // @run-at          document-end
 
 // @include         http*://*imdb.com/*
@@ -16,31 +16,20 @@ GM_addStyle(".copy-button { color: gold; cursor: pointer; font-weight: bold; } "
 
 var movie = JSON.parse(document.querySelector("script[type='application/ld+json']").textContent);
 var title = [].reduce.call(document.querySelector("h1").childNodes, function(a, b) { return a + (b.nodeType === 3 ? b.textContent : ''); }, '').trim();
-var description = ` — ${movie.genre.join ? movie.genre.join(", ") : movie.genre}, ${movie.duration.replace("PT", "").replace("H", ":").replace("M","h")}`;
+var result = `- [${title}](${window.location}) (${movie.genre.join ? movie.genre.join(", ") : movie.genre}, ${movie.duration.replace("PT", "").replace("H", ":").replace("M","h")}) ()`;
 
 // clipboard textarea
-var clip = document.createElement('div');
+var clip = document.createElement('textarea');
 document.body.appendChild(clip);
-var linkNode = document.createElement('a');
-linkNode.href = document.location.toString();
-var descriptionNode = document.createElement('span');
-clip.appendChild(linkNode);
-clip.appendChild(descriptionNode);
-
-var range = document.createRange();
-range.selectNode(clip);
 
 // copy button
 var button = document.createElement("span");
 button.innerText = "copy";
 button.className = "copy-button";
 button.addEventListener("click", (event) => {
-    linkNode.innerText = title;
-    descriptionNode.innerText = description;
-
-    window.getSelection().removeAllRanges(range);
-    window.getSelection().addRange(range);
-    document.execCommand('copy');
+  clip.value = result;
+  clip.select();
+  document.execCommand('copy');
 });
 
 // visual separator
@@ -49,4 +38,3 @@ separator.innerText = " | ";
 
 document.querySelector(".subtext").appendChild(separator);
 document.querySelector(".subtext").appendChild(button);
-
