6603ede0558f9db03a6bbea51f69e3c0cdaf36b3
Darek Kay <hello@darekkay.com>
2022-05-03 15:10:13 +0200

    Update brew apps

--- mac/setup.sh
+++ mac/setup.sh
@@ -3,5 +3,8 @@
 # Delta: https://github.com/dandavison/delta
 brew install git-delta
 
+# mmv: https://formulae.brew.sh/formula/mmv
+brew install mmv
+
 # Fonts
 brew install --cask font-aldrich font-anaheim font-anton font-assistant font-cuprum font-dosis font-fira-code font-gudea font-lato font-noto-sans font-open-sans font-oswald font-redacted-script
