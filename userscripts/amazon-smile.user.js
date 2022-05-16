48097bdb1afc27d22a44b040569082a2766a99df
Darek Kay <hello@darekkay.com>
2022-05-16 16:44:08 +0200

    Update Amazon redirect userscript

--- userscripts/amazon-smile.user.js
+++ userscripts/amazon-smile.user.js
@@ -1,25 +1,33 @@
 // ==UserScript==
 // @name            Amazon Smile Redirect
-// @version         1.1.0
+// @version         1.2.0
 // @description     Redirect Amazon to Amazon Smile
 // @author          Darek Kay <hello@darekkay.com>
 // @namespace       https://darekkay.com
 // @downloadURL     https://github.com/darekkay/config-files/raw/master/userscripts/amazon-smile.user.js
 // @include         http*://amazon.de/*
 // @include         http*://www.amazon.de/*
-// @exclude         http*://smile.amazon.*
-// @exclude         http*://www.smile.amazon.*
+// @include         http*://smile.amazon.*
+// @include         http*://www.smile.amazon.*
 // @grant       none
 // ==/UserScript==
 
-// smile only works for logged-in users
-if(!document.querySelector("a[href^='https://www.amazon.de/ap/signin'")) {
+let redirectUrl = window.location.href;
 
+// smile only works for logged-in users
+if(!document.querySelector("a[href^='https://www.amazon.de/ap/signin'") && !redirectUrl.includes("smile.amazon")) {
   var url = window.location.href;
-  var redirectUrl = url
+  redirectUrl = url
     .replace("//www.amazon", "//smile.amazon")
     .replace("//amazon.", "//smile.amazon.");
+}
 
-  console.log("Redirecting to Amazon Smile: " + redirectUrl);
+const productPageMatch = redirectUrl.match("smile\.amazon\.(.*)/(.*)\/dp\/");
+if (productPageMatch) {
+    redirectUrl = redirectUrl.replace(`/${productPageMatch[productPageMatch.length - 1]}`, "")
+}
+
+if (redirectUrl !== window.location.href) {
+  console.log("Redirecting to: " + redirectUrl);
   window.location.replace(redirectUrl);
 }
