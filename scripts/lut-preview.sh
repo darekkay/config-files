920785de082575ad4bd27c38a3d8819375951774
Darek Kay <hello@darekkay.com>
2022-07-12 15:22:07 +0200

    Update XYplorer scripts

--- scripts/lut-preview.sh
+++ scripts/lut-preview.sh
@@ -1,9 +1,21 @@
-TEST_IMAGE_FOLDER=/c/images
+#!/usr/bin/env bash
+
+set -e           # exit on error
+set -o pipefail  # trace ERR through pipes
+set -o errtrace  # trace ERR through 'time command' and other functions
+set -o errexit   # exit the script if any statement returns a non-true return value
+
+# This script applies all LUT files from the current folder to all test images
+
+TEST_IMAGE_FOLDER=/d/misc/photography/LUTs/_test-img/
 
 find $TEST_IMAGE_FOLDER -name "*.jpg"|while read image; do
   cp "$image" .
   find . -name "*.cube"|while read lut; do
-    # gmic "$image" "$lut" map_clut[0] [1] o[0] \"$(basename -s .cube "$lut")-$(basename -s .jpg "$image").jpg\"
-    gmic "$image" "$lut" map_clut[0] [1] o[0] \"$(basename -s .jpg "$image")-$(basename -s .cube "$lut").jpg\"
+    filename_lut_img=$(basename -s .cube "$lut")-$(basename -s .jpg "$image").jpg
+    filename_img_lut=$(basename -s .jpg "$image")-$(basename -s .cube "$lut").jpg
+    gmic "$image" "$lut" map_clut[0] [1] o[0] \"$filename_lut_img\"
+    # Uncomment to create two duplicate images to view the results a) by LUT b) by test image
+    # cp "$filename_lut_img" "$filename_img_lut"
   done
 done
